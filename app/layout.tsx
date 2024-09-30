74637937d083b7eaf3ab4c2c7ca57f23a5ce8af3
mehran <mehran@chartbox.dev>
2024-09-30 10:28:13 +0330

    feat (analytics): add speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
 import './globals.css';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const poppins = Poppins({
   subsets: ['latin'],
